a17b9965882da3e55ba43ce61206d2a88febcc7e
sunsunnp <sunsun.np@gmail.com>
2020-04-08 12:07:43 +0700

    change design range

--- src/pages/BranchSetting.vue
+++ src/pages/BranchSetting.vue
@@ -5,31 +5,31 @@
 
 				<div class="row">
 				<div class="col-3">
-				<div class="slider" id="demo-slider">
-					<div class="thumb" id="demo-thumb"></div>
+				<!-- <div class="slider-wrapper">
+					<input type="range" min="0" max="11" value="7" step="1">
 				</div>
- 				<div class="value" id="demo-value">Lotus</div>
+ 				<div class="" id="demo-value">Lotus</div> -->
 				</div>
 
 				<div class="col-3">
-				<div class="slider" id="demo-slider">
-					<div class="thumb" id="demo-thumb"></div>
+				<div class="slider-wrapper">
+					<input type="range" min="0" max="11" value="1" step="1" disabled>
 				</div>
  				<div class="value" id="demo-value">Sansiri</div>
 				</div>
 
 				<div class="col-3">
-				<div class="slider" id="demo-slider">
-					<div class="thumb" id="demo-thumb"></div>
-				</div>
- 				<div class="value" id="demo-value">Major</div>
+					<div class="slider-wrapper">
+						<input type="range" min="0" max="11" value="5" step="1">
+					</div>
+ 					<div class="value" id="demo-value">Major</div>
 				</div>
 
 				<div class="col-3">
-				<div class="slider" id="demo-slider">
-					<div class="thumb" id="demo-thumb"></div>
-				</div>
- 				<div class="value" id="demo-value">MTL</div>
+					<div class="slider-wrapper">
+						<input type="range" min="0" max="11" value="10" step="1">
+					</div>
+					<div class="value" id="demo-value">MTL</div>
 				</div>
 				</div>
  
@@ -69,10 +69,29 @@
  
 .value {
     position: absolute;
-    left: 83px;
-	top: 365px; 
+	left: -45px;
+	top: 320px; 
     width: 80px;
     text-align: center;
     font-family: "Lucida Console", Monaco, monospace;
+}
+
+.slider-wrapper {
+  display: inline-block;
+  width: 20px;
+  height: 150px;
+  padding: 0;
+}
+
+.slider-wrapper input {
+  width: 250px;
+  height: 20px;
+  margin: 0;
+  transform-origin: 75px 75px;
+  transform: rotate(-90deg);
+  cursor: pointer; 
+    position: absolute;
+  left: -18px; 
+	top: 150px; 
 }
 </style>
